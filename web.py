4843a2fd8ac6ce70b60c83291ddbed3f4046bb9b
MiaTian1024 <sddxtm123@gmail.com>
2023-04-29 22:19:04 +1200

    clear text input field

--- web.py
+++ web.py
@@ -9,7 +9,7 @@ def add_todo():
     todo = st.session_state["new_todo"] +"\n"
     todos.append(todo)
     functions.write_todos(todos)
-
+    st.session_state["new_todo"] = "" # clear the text input field
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
